cc431dea2befa934a6602febd73def9296182b50
jesmary885 <jesmary885@gmail.com>
2025-05-17 18:24:09 -0400

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -72,8 +70,5 @@
             toastr.error(ms)
         })
     </script>
-
-
-
     </body>
 </html>
